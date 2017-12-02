a56fe2a15116095a5988f23e04424a87d17ff7b4
John Paul <jpcode@Johns-MacBook-Air.local>
2017-12-02 17:25:43 -0500

    task

---
+++ class2/exercises/tasks.js
@@ -0,0 +1,5 @@
+function sumaArr( arr ) {
+	// body...
+}
+
+console.log(sumaArr([3,2,1,4,5] ) );
--- class2/index.js
+++ class2/index.js
@@ -1,1 +1,15 @@
-console.log("aa");
+var arreglo = [1,2,3];
+var suma = arreglo[0] + arreglo[2];
+
+//imprimir todos los valores del arreglo
+
+for ( var i = 0; i < arreglo.length; i++ ){
+	//console.log( arreglo[ i ] )
+}
+//puede guardar cualquier tipo de dato
+var arr2 = ["string",2,2.3];
+
+
+
+
+
